25b68f28a2029ed884960c858a4349c85f927b89
Yumax-panda <yuma0wall@gmail.com>
2024-07-30 14:45:19 +0900

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,15 @@
+//
+//  ChatPrototypeApp.swift
+//  Copyright Yumax-panda. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
